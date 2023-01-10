c2ede56baab03b5aebc3d949cb12709acb4b6d8c
vedant jaiswal <vedantjaiswal1@gmail.com>
2023-01-10 21:40:50 +0530

    add javascript shorthand include

---
+++ InstagramReel/JavascriptShorthand(include).js
@@ -0,0 +1,19 @@
+const userAccess = (userRole) => {
+  if (
+    userRole === "admin" ||
+    userRole === "manager" ||
+    userRole === "editor" ||
+    userRole === "ownwer"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const userAccess = (userRole) => {
+  const roles = ["admin", "manager", "editor", "owner"];
+  return roles.includes(userRole);
+};
+
+console.log(userAccess("admin"));
